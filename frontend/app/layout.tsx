38376ba212c3a9ed8e1e5d159a422ab5b896b026
胡时利Andrew <820643316@qq.com>
2026-04-22 09:09:18 +0800

    0422 1

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,5 @@
 ﻿import React from "react"
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 import 'katex/dist/katex.min.css'
@@ -8,9 +7,6 @@ import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import "@react-pdf-viewer/search/lib/styles/index.css";
 import { ThemeProvider } from "@/components/theme-provider"
-
-const _geist = Geist({ subsets: ["latin"] });
-const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: '数字化管控软件',
